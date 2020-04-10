dafad3d792980885250f363f0980388fedfa49fd
azureuz <chinmayshekatkar12@gmail.com>
2020-04-10 22:32:47 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+var path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.sendFile(path.join(__dirname, "public","e.html"));
+    //res.sendFile(path.join(__dirname + '/e.html'));
 });
 
 app.listen(3000, function () {
